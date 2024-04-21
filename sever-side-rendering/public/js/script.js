28522c572e4b271e9267ede1fd176e6e1424f4f2
Tran Cao Hieu <96785351+hieutran03@users.noreply.github.com>
2024-04-21 20:24:08 +0700

    Delete sever-side-rendering directory

--- sever-side-rendering/public/js/script.js
+++
@@ -1,7 +0,0 @@
-const logout_form = document.querySelector('#logout_form');
-if(logout_form){
-  const logout_btn = document.querySelector('#logout_btn');
-  logout_btn.addEventListener('click', (e)=>{
-    logout_form.submit();
-  })
-}
